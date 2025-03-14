e07fadb836be3fe27ac74bc179087272a738b8ab
Giovanni Familiari <giovannifamiliari180@gmail.com>
2025-03-14 16:19:17 +0100

    Changed Firebase due to exposed API Key :)

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCuUwL5yExS2QEingiiGSULY1U_5TPNYvo",
-    authDomain: "gameportal-saw.firebaseapp.com",
-    projectId: "gameportal-saw",
-    storageBucket: "gameportal-saw.firebasestorage.app",
-    messagingSenderId: "832534611754",
-    appId: "1:832534611754:web:5bbfea21fd70a66d028053",
-    measurementId: "G-WF03LSQ1X2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-export { app, auth };
+export {app, auth};
