5dc21b7cda0e6f58db81ba23b0b7982c7d4645c3
Giovanni Familiari <giovannifamiliari180@gmail.com>
2025-03-24 22:19:11 +0100

    typo

--- components/pages/SettingsPage.jsx
+++ components/pages/SettingsPage.jsx
@@ -2,7 +2,7 @@ import {Button, Modal, Toast, ToastContainer} from "react-bootstrap";
 import React, {useState} from "react";
 import {deleteUser, updateNotificationPreferences, updateUsername} from "../../firebase/firestore.js";
 import {removeUser} from "../../firebase/auth.js";
-import "/src/css/setttings.css"
+import "/src/css/settings.css"
 
 export default function SettingsPage({setShowSettings, showSettings, userDb, setUserDb, setLoggedIn}) {
 
