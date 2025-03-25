e844c2ba7c1eb2d62643200b38a9108f0b127480
Giovanni Familiari <giovannifamiliari180@gmail.com>
2025-03-25 20:09:41 +0100

    made pwa installable

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+            enabled: true,
+        },
+        manifest: {
+            name: "GamePortal - Un portale nel mondo dei videogiochi",
+            short_name: "GamePortal",
+            description: "Un portale nel mondo dei videogiochi",
+            start_url: "/",
+            display: "standalone",
+            background_color: "#262626",
+            theme_color: "#12bee6",
+            icons: [
+                {
+                    src: "/icon-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    src: "/icon-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                },
+            ],
+        },
+    }),
+    ],
     server: {
         proxy: {
             '/steam': {
