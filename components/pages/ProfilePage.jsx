62aa8aec2f1cd17893a11102b7c37f4777c6b1c2
Giovanni Familiari <giovannifamiliari180@gmail.com>
2025-04-09 23:17:59 +0200

    Added loading component

--- components/pages/ProfilePage.jsx
+++ components/pages/ProfilePage.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from "react";
 import {getSteamUserInfo} from "../../external/steamApi.js";
-import {Col, Row} from "react-bootstrap";
+import {Col, Row, Spinner} from "react-bootstrap";
+import {isObjectEmpty} from "../../src/util.js";
+import "../../src/css/profilePage.css"
 
 export default function ProfilePage({userDb}) {
     const [steamUserData, setSteamUserData] = useState({})
@@ -9,7 +11,6 @@ export default function ProfilePage({userDb}) {
         let initSteamUserData = async () => {
             await getSteamUserInfo(userDb.username).then(userData => {
                 setSteamUserData(userData);
-                console.log(userData)
             });
         }
         initSteamUserData();
@@ -17,17 +18,30 @@ export default function ProfilePage({userDb}) {
 
     return (
         <>
-            <Row>
-                <h4>Profilo di {steamUserData.userInfo?.personaname}</h4>
-            </Row>
-            <Row className="pt-4">
-                <Col md={2}>
-                    {/*<Image src={steamUserData.userInfo?.avatarfull} rounded={true}/>*/}
-                </Col>
-                <Col>
+            {isObjectEmpty(steamUserData) ? (
+                    <>
+                        <Row className={"d-flex justify-content-center"}>
+                            <Col md={"4"} offset-md={"4"}
+                                 className={"loading-holder d-flex align-items-center justify-content-center"}>
+                                <Spinner animation="border" variant="info"/>&nbsp;&nbsp;Caricamento dati del profilo Steam
+                            </Col>
+                        </Row>
+                    </>
+                ) :
+                <>
+                    <Row>
+                        <h4>Profilo di {steamUserData.userInfo.steamUsername}</h4>
+                    </Row>
+                    <Row className="pt-4">
+                        <Col md={2}>
+                            {/*<Image src={steamUserData.userInfo.steamAvatar} rounded={true}/>*/}
+                        </Col>
+                        <Col>
 
-                </Col>
-            </Row>
+                        </Col>
+                    </Row>
+                </>
+            }
         </>
 
     )
